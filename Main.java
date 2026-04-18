d7239e6b57b801c523663708e6acad910d9ab4cc
superIdol <abellabenmar@gmail.com>
2026-04-18 18:15:00 +0800

    Commit

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
