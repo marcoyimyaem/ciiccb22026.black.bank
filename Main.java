ce18ed3ec0dbe68757d096ed49a565e633fb018e
Justine Jerald Baliguat <94623035+jjybaliguat@users.noreply.github.com>
2026-04-18 18:20:53 +0800

    some changes

--- Main.java
+++ Main.java
@@ -108,100 +108,100 @@ public class Main {
     //team sibuyas eof
 
     
-}
-//team ba
-    class Console {
-        Scanner sc = new Scanner(System.in);
-
-        public void displayMenu(){
-            System.out.println("<<<<< Welcome To Black Banking System >>>>>>");
-            System.out.println("Options: ");
-            System.out.println("1: Login");
-            System.out.println("2: Exit");
-            System.out.println("Enter your choice: ");
-            String choice = sc.nextLine();
-
-            switch (choice) {
-                case "1":
-                    displayLogin();
-                    break;
-                case "2":
-                    System.out.println("Thank you for using our banking system. Exiting....");
-                    System.exit(0);
-            }
-        }
-
-        public void displayLogin(){
-            Scanner sc = new Scanner(System.in);
-            final int MAX_LOGIN_LIMIT = 3;
-            boolean isSuccessLogin = false;
-            int numTries = 0;
-            while(true){
-                if(numTries <= MAX_LOGIN_LIMIT){
-                    System.out.println("Please enter username: ");
-                    String username = sc.nextLine();
-                    System.out.println("Please enter your password: ");
-                    String userPin = sc.nextLine();
-                    UserAuth user = Auth.login(username, userPin);
-                    if(user !== null){
-                        displayDashboard(user);
-                        break;
-                    }else{
-                        System.out.println("Invalid username or password");
-                        numTries++;
+        //team ba
+            class Console {
+                Scanner sc = new Scanner(System.in);
+        
+                public void displayMenu(){
+                    System.out.println("<<<<< Welcome To Black Banking System >>>>>>");
+                    System.out.println("Options: ");
+                    System.out.println("1: Login");
+                    System.out.println("2: Exit");
+                    System.out.println("Enter your choice: ");
+                    String choice = sc.nextLine();
+        
+                    switch (choice) {
+                        case "1":
+                            displayLogin();
+                            break;
+                        case "2":
+                            System.out.println("Thank you for using our banking system. Exiting....");
+                            System.exit(0);
                     }
-                }else{
-                    System.out.println("Sorry your ip is blocked!");
-                    break;
+                }
+        
+                public void displayLogin(){
+                    Scanner sc = new Scanner(System.in);
+                    final int MAX_LOGIN_LIMIT = 3;
+                    boolean isSuccessLogin = false;
+                    int numTries = 0;
+                    while(true){
+                        if(numTries <= MAX_LOGIN_LIMIT){
+                            System.out.println("Please enter username or number: ");
+                            String username = sc.nextLine();
+                            System.out.println("Please enter your password: ");
+                            String userPin = sc.nextLine();
+                            UserAuth user = LoginValidation.validate(username, userPin);
+                            if(user !== null){
+                                displayDashboard(user);
+                                break;
+                            }else{
+                                System.out.println("Invalid username or password");
+                                numTries++;
+                            }
+                        }else{
+                            System.out.println("Sorry your ip is blocked!");
+                            break;
+                        }
+                    }
+                }
+        
+                public void displayDashboard(UserAuth user){
+                    while(true){
+                        System.out.println("Welcome " + user.getUserName());
+                        System.out.println("Options: ");
+                        System.out.println("1: Check Balance");
+                        System.out.println("2: Deposit");
+                        System.out.println("3: Change Pin");
+                        System.out.println("4: Exit");
+        
+                        String input = sc.nextLine();
+        
+                        switch (input) {
+                            case "1":
+                                System.out.println("Your current balance is: " + user.getUserBalance());
+                                break;
+                            case "2":
+                                System.out.println("Please enter amount to deposit: ");
+                                double amount = sc.nextDouble();
+                                deposit(amount, user);
+                                break;
+                            case "3":
+                                System.out.println("Please enter old pin: ");
+                                String oldPin = sc.nextLine();
+                                System.out.println("Enter new pin: ");
+                                String newPin = sc.nextLine();
+                                validatePin(oldPin, newPin, user);
+                                break;
+                            case "4":
+                                System.exit(0);
+                            default:
+                                break;
+                        }
+                    }
+                }
+        
+                public void deposit(double amount, UserAuth user){
+                    user.setUserBalance(amount);
+                }
+        
+                public void validatePin(String oldPin, String newPin, UserAuth user){
+                    if(!user.getUserPin().equals(oldPin))
+                        System.out.println("Old pin not matched!");
+                    else
+                        user.setUserPin(newPin);
                 }
             }
-        }
-
-        public void displayDashboard(UserAuth user){
-            while(true){
-                System.out.println("Welcome " + user.getUserName());
-                System.out.println("Options: ");
-                System.out.println("1: Check Balance");
-                System.out.println("2: Deposit");
-                System.out.println("3: Change Pin");
-                System.out.println("4: Exit");
-
-                String input = sc.nextLine();
-
-                switch (input) {
-                    case "1":
-                        System.out.println("Your current balance is: " + user.getUserBalance());
-                        break;
-                    case "2":
-                        System.out.println("Please enter amount to deposit: ");
-                        double amount = sc.nextDouble();
-                        deposit(amount, user);
-                        break;
-                    case "3":
-                        System.out.println("Please enter old pin: ");
-                        String oldPin = sc.nextLine();
-                        System.out.println("Enter new pin: ");
-                        String newPin = sc.nextLine();
-                        validatePin(oldPin, newPin, user);
-                        break;
-                    case "4":
-                        System.exit(0);
-                    default:
-                        break;
-                }
-            }
-        }
-
-        public void deposit(double amount, UserAuth user){
-            user.setUserBalance(amount);
-        }
-
-        public void validatePin(String oldPin, String newPin, UserAuth user){
-            if(!user.getUserPin().equals(oldPin))
-                System.out.println("Old pin not matched!");
-            else
-                user.setUserPin(newPin);
-        }
-    }
-//team ba eof
+        //team ba eof
+}
 
